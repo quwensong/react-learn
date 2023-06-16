7a5b87e879fe183c5921835a0b39da630ee4d6f7
quwensong <1607186824@qq.com>
2023-06-16 15:48:43 +0800

    ✨ feat:

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,4 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>11111</div>);
+
+const jsx = <div>11111</div>;
+
+console.log(JSON.stringify(jsx, null, 2));
+
+root.render(jsx);
+
+var lengthOfLongestSubstring = function (s) {
+  let arr = [],
+    max = 0;
+  for (let i = 0; i < s.length; i++) {
+    let index = arr.indexOf(s[i]);
+    if (index !== -1) {
+      arr.splice(0, index + 1);
+    }
+    arr.push(s.charAt(i));
+    max = Math.max(arr.length, max);
+  }
+  return max;
+};
+
+console.log(lengthOfLongestSubstring("dvdf"));
