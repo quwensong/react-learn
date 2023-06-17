75ce8516e881b326433caf612c56313ed8615ace
quwensong <1607186824@qq.com>
2023-06-17 08:27:52 +0800

    ✨ feat: 1

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,26 +1,17 @@
+// 对es6内置api进行兼容性处理
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// import "@/index.less";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const jsx = <div>11111</div>;
+const jsx = <div> 11111 </div>;
 
 console.log(JSON.stringify(jsx, null, 2));
 
 root.render(jsx);
-
-var lengthOfLongestSubstring = function (s) {
-  let arr = [],
-    max = 0;
-  for (let i = 0; i < s.length; i++) {
-    let index = arr.indexOf(s[i]);
-    if (index !== -1) {
-      arr.splice(0, index + 1);
-    }
-    arr.push(s.charAt(i));
-    max = Math.max(arr.length, max);
-  }
-  return max;
-};
-
-console.log(lengthOfLongestSubstring("dvdf"));
